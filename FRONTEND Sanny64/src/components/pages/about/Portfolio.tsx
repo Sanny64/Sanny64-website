b3103f9c87db89937d8f4c5fceeb22c94c729ddf
Sanny <santino.pia04@gmx.de>
2025-10-13 18:03:08 +0200

    Fixed misinfo; had a thought fart :D

--- FRONTEND Sanny64/src/components/pages/about/Portfolio.tsx
+++ FRONTEND Sanny64/src/components/pages/about/Portfolio.tsx
@@ -1,7 +1,7 @@
 export default function Portfolio() {
     return (
         <div className="confidential">
-          This page will contain private info, that can only be accessed while logged in. All private info must be handled confidentially and will therefore be saved as github env secrets.
+          This page will contain private info. All private info must be handled confidentially and will therefore be saved as secrets.
         </div>
     )
 }
